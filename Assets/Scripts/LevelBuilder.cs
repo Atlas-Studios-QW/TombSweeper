68ea331df36e0eba0f72ad791ba6f1943fa03c08
QuillanW <quillanwielhouwer@live.nl>
2022-10-14 23:15:19 +0200

    update

--- Assets/Scripts/LevelBuilder.cs
+++ Assets/Scripts/LevelBuilder.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using TMPro;
 using UnityEngine.SceneManagement;
 using Unity.VisualScripting;
@@ -33,13 +34,21 @@ public class LevelBuilder : MonoBehaviour
     public GameObject AlertBox;
     public GameObject CoinBox;
     public GameObject ItemBox;
+    public GameObject ItemDetails;
     public GameObject ObjectiveBox;
     public GameObject PausePanel;
     public GameObject DeathPanel;
 
+    [Header("-----Sprites")]
+    public Sprite CoinImg;
+    public Sprite ItemImg;
+    public Sprite KeyImg;
+
     [Header("-----Items")]
     public GameObject IconParent;
     public List<Item> Items = new List<Item>();
+    public List<Sprite> KeySprites = new List<Sprite>();
+    public GameObject CompassMarker;
 
     [Header("-----Settings")]
     public int BombChance = 25;
@@ -47,7 +56,7 @@ public class LevelBuilder : MonoBehaviour
     public int ItemChance = 1;
 
     [Header("-----Dev settings")]
-    public bool EnableColors = false;
+    public bool EnableBombSight = false;
     public float[] HexMovement = { 3.9f, 4.5f };
 
     [HideInInspector]
@@ -64,17 +73,24 @@ public class LevelBuilder : MonoBehaviour
     private List<int> BombRooms = new List<int>();
     private List<int> CoinRooms = new List<int>();
     private List<int> ItemRooms = new List<int>();
+    private List<int> KeyRoomsID = new List<int>();
+    private List<Vector2> KeyRooms = new List<Vector2>(); 
 
     private List<Item> CollectedItems = new List<Item>();
 
+    private int CurrentMove = 0;
     private int CurrentRoomID = 1;
     private int TotalCoins = 0;
     private int CounterFixObjective = 0;
     private int ItemTotal = 0;
+    private int KeyTotal = 0;
+    private bool EnableSight = false;
+    private bool HasCompass = false;
 
     private void Start()
     {
         LoadNewRooms();
+        ChoosePositions();
         Alert("Objective:\nFind the key pieces [0/3]");
     }
 
@@ -84,11 +100,43 @@ public class LevelBuilder : MonoBehaviour
         {
             PausePanel.SetActive(!PausePanel.activeSelf);
         }
+
+        string ItemName = IsPointerOverUIElement();
+        if (ItemName != null)
+        {
+            Item CurrentItem = null;
+            foreach (Item item in Items)
+            {
+                if (item.name == ItemName)
+                {
+                    CurrentItem = item;
+                }
+            }
+
+            ItemDetails.transform.position = Input.mousePosition;
+            ItemDetails.transform.Find("Title").GetComponent<TextMeshProUGUI>().text = ItemName;
+            ItemDetails.transform.Find("Details").GetComponent<TextMeshProUGUI>().text = CurrentItem.description;
+        }
+        else
+        {
+            ItemDetails.transform.position = new Vector2(-1000,0);
+        }
     }
 
     public void ResumeGame()
     {
         PausePanel.SetActive(false);
+    }
+
+    public void ChoosePositions()
+    {
+        for (int i = 1; i < 5; i++)
+        {
+            float NewKeyLocationX = Random.Range(-10 * i, 10 * i) * HexMovement[0];
+            float NewKeyLocationY = Random.Range(-10 * i, 10 * i) * HexMovement[1];
+            KeyRooms.Add(new Vector2(NewKeyLocationX, NewKeyLocationY));
+            //print(new Vector2(NewKeyLocationX, NewKeyLocationY));
+        }
     }
 
     public void LoadNewRooms()
@@ -97,6 +145,11 @@ public class LevelBuilder : MonoBehaviour
         
         int CurrentRoom = 0;
         bool Dead = false;
+
+        if (HasCompass && CurrentMove % 3 == 0)
+        {
+            StartCoroutine(CompassBlink());
+        }
 
         if (Rooms.IndexOf(PlayerPos) == -1)
         {
@@ -127,24 +180,55 @@ public class LevelBuilder : MonoBehaviour
             EndGame();
             Dead = true;
         }
+        else if (KeyRoomsID.Contains(CurrentRoom))
+        {
+            GameObject.Find("Room" + CurrentRoom).transform.Find("Canvas").Find("Icon").GetComponent<Image>().color = new Color(0, 0, 0, 0);
+            KeyRoomsID.RemoveAt(KeyRoomsID.IndexOf(CurrentRoom));
+            KeyTotal++;
+            if (KeyTotal == 3)
+            {
+                Alert($"Objective:\nFind the exit");
+            }
+            else
+            {
+                Alert($"Objective:\nFind the key pieces [{KeyTotal}/3]");
+            }
+        }
         else if (CoinRooms.Contains(CurrentRoom))
         {
+            GameObject.Find("Room" + CurrentRoom).transform.Find("Canvas").Find("Icon").GetComponent<Image>().color = new Color(0,0,0,0);
             CoinRooms.RemoveAt(CoinRooms.IndexOf(CurrentRoom));
             TotalCoins++;
             Alert("Coin");
         }
         else if (ItemRooms.Contains(CurrentRoom))
         {
+            GameObject.Find("Room" + CurrentRoom).transform.Find("Canvas").Find("Icon").GetComponent<Image>().color = new Color(0, 0, 0, 0);
             ItemRooms.RemoveAt(ItemRooms.IndexOf(CurrentRoom));
             if (ItemTotal < 3)
             {
                 CollectedItems.Add(Items[ItemTotal]);
                 Alert("Item");
+                Alert("You collected an item!");
+
+                if (ItemTotal == 1)
+                {
+                    EnableSight = true;
+                }
+                else if (ItemTotal == 2)
+                {
+                    HasCompass = true;
+                }
+                else if (ItemTotal == 3)
+                {
+                    
+                }
             }
         }
 
         int NewBombs = 0;
         int BombsFound = 0;
+        bool KeyRoomFound = false;
 
         if (!Dead)
         {
@@ -163,12 +247,30 @@ public class LevelBuilder : MonoBehaviour
                     if (CurrentRoomID <= 6) { SpawnBomb = false; }
                     else { SpawnBomb = Random.Range(0, 100) > (100 - BombChance); }
 
-                    if (SpawnBomb && BombsFound != 3)
+                    float KeyMinDistance = Mathf.Infinity;
+                    foreach (Vector2 KeyRoom in KeyRooms)
+                    {
+                        float Distance = Vector2.Distance(KeyRoom, PlayerPos);
+                        if (Distance < KeyMinDistance)
+                        {
+                            KeyMinDistance = Distance;
+                        }
+                    }
+                    //print(KeyMinDistance);
+
+                    if (KeyMinDistance < 3.0f && !KeyRoomFound)
+                    {
+                        KeyRoomFound = true;
+                        NewRoom.transform.Find("Canvas").Find("Icon").GetComponent<Image>().color = new Color(255, 255, 255, 1);
+                        NewRoom.transform.Find("Canvas").Find("Icon").GetComponent<Image>().sprite = KeyImg;
+                        KeyRoomsID.Add(CurrentRoomID);
+                    }
+                    else if (SpawnBomb && BombsFound != 3)
                     {
                         NewBombs++;
                         BombsFound++;
                         BombRooms.Add(CurrentRoomID);
-                        if (EnableColors)
+                        if (EnableBombSight)
                         {
                             NewRoom.GetComponent<SpriteRenderer>().material.color = new Color(255, 0, 0);
                         }
@@ -177,17 +279,19 @@ public class LevelBuilder : MonoBehaviour
                     else if (Random.Range(0,100) > (100 - CoinChance))
                     {
                         CoinRooms.Add(CurrentRoomID);
-                        if (EnableColors)
+                        if (EnableSight)
                         {
-                            NewRoom.GetComponent<SpriteRenderer>().material.color = new Color(0, 255, 0);
+                            NewRoom.transform.Find("Canvas").Find("Icon").GetComponent<Image>().color = new Color(255, 255, 255, 1);
+                            NewRoom.transform.Find("Canvas").Find("Icon").GetComponent<Image>().sprite = CoinImg;
                         }
                     }
                     else if (Random.Range(0, 100) > (100 - ItemChance) && ItemTotal < 3)
                     {
                         ItemRooms.Add(CurrentRoomID);
-                        if (EnableColors)
+                        if (EnableSight)
                         {
-                            NewRoom.GetComponent<SpriteRenderer>().material.color = new Color(0, 0, 255);
+                            NewRoom.transform.Find("Canvas").Find("Icon").GetComponent<Image>().color = new Color(255, 255, 255, 1);
+                            NewRoom.transform.Find("Canvas").Find("Icon").GetComponent<Image>().sprite = ItemImg;
                         }
                     }
 
@@ -213,11 +317,33 @@ public class LevelBuilder : MonoBehaviour
         }
 
         //print("Next move ======================================================");
+
+        CurrentMove++;
     }
 
     public void Alert(string Message)
     {
         StartCoroutine(Alerter(Message));
+    }
+
+    private IEnumerator CompassBlink()
+    {
+        CompassMarker.transform.rotation = new Quaternion(0,0,,0);
+
+        Image Marker = CompassMarker.transform.Find("Marker").GetComponent<Image>();
+        for (int i = 0; i < 4; i++)
+        {
+            while (Marker.color.a < 1.0f)
+            {
+                Marker.color += new Color(0, 0, 0, 0.025f);
+                yield return null;
+            }
+            while (Marker.color.a > 0.0f)
+            {
+                Marker.color -= new Color(0, 0, 0, 0.025f);
+                yield return null;
+            }
+        }
     }
 
     private IEnumerator Alerter(string Message)
@@ -260,7 +386,7 @@ public class LevelBuilder : MonoBehaviour
             GameObject NewIcon = Instantiate(IconPrefab, IconParent.transform);
             NewIcon.transform.position += new Vector3(0, 150 * ItemTotal, 0);
             NewIcon.transform.Find("Icon").GetComponent<Image>().sprite = CollectedItems[ItemTotal].icon;
-            NewIcon.name = CollectedItems[ItemTotal].name;
+            NewIcon.transform.Find("Icon").name = CollectedItems[ItemTotal].name;
             ItemTotal++;
         }
         else
@@ -274,5 +400,30 @@ public class LevelBuilder : MonoBehaviour
     private void EndGame()
     {
         DeathPanel.SetActive(true);
+    }
+
+    public string IsPointerOverUIElement()
+    {
+        return IsPointerOverUIElement(GetEventSystemRaycastResults());
+    }
+
+    private string IsPointerOverUIElement(List<RaycastResult> eventSystemRaysastResults)
+    {
+        for (int index = 0; index < eventSystemRaysastResults.Count; index++)
+        {
+            RaycastResult curRaysastResult = eventSystemRaysastResults[index];
+            if (curRaysastResult.gameObject.tag == "ItemUI")
+                return curRaysastResult.gameObject.name;
+        }
+        return null;
+    }
+
+    static List<RaycastResult> GetEventSystemRaycastResults()
+    {
+        PointerEventData eventData = new PointerEventData(EventSystem.current);
+        eventData.position = Input.mousePosition;
+        List<RaycastResult> raysastResults = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, raysastResults);
+        return raysastResults;
     }
 }
--- Assets/Scripts/Menus.cs
+++ Assets/Scripts/Menus.cs
@@ -15,22 +15,4 @@ public class Menus : MonoBehaviour
     {
         Application.Quit();
     }
-
-    private void Update()
-    {
-        print(CheckItem());
-    }
-
-    public string CheckItem()
-    {
-        int layerMask = 1 << 5;
-        Ray ray;
-        RaycastHit hit;
-        ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        if (Physics2D.Raycast(ray, out hit, Mathf.Infinity, layerMask))
-        {
-            return hit.transform.gameObject.name;
-        }
-        return null;
-    }
 }
