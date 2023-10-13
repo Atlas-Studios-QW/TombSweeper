64a2f42ed4547a323bfc09714db0367592e3fcdd
QuillanW <56843492+QuillanW@users.noreply.github.com>
2023-10-13 22:28:15 +0200

    Update

--- Scripts/MovementController.gd
+++ Scripts/MovementController.gd
@@ -4,6 +4,7 @@ var playerSpeed
 var tileMap
 
 var moveButtons
+var currentCellLabel
 
 var fromPosition = Vector2(0,0)
 var nextPosition = Vector2(0,0)
@@ -13,9 +14,13 @@ func _ready():
 	playerSpeed = get_node("/root/GameData").get("playerSpeed")
 	tileMap = get_node("/root/Level/World/TileMap")
 	moveButtons = get_node("MoveControl").get_children()
+	currentCellLabel = get_node("OtherUI/CurrentCellLabel")
 	var spawnPoint = get_node("/root/GameData").get("spawnPoint")
 	
-	position = tileMap.GetGlobalPos(spawnPoint)
+	position = tileMap.GetGlobalPos(spawnPoint) + Vector2(0,16)
+	fromPosition = position
+	nextPosition = position
+	UpdateCellIndicators()
 	pass
 
 func _process(delta):
@@ -25,7 +30,10 @@ func _process(delta):
 		movement = minf(movement,1.0)
 		position = fromPosition.lerp(nextPosition, movement)
 	else:
-		moveButtons[0].get_parent().show()
+		if (movement != 2.0):
+			movement = 2.0
+			moveButtons[0].get_parent().show()
+			tileMap.OnEnterCell(position + Vector2(32,32))
 	pass
 
 func StartMove(direction: int = 0):
@@ -34,15 +42,21 @@ func StartMove(direction: int = 0):
 	
 	moveButtons[0].get_parent().hide()
 	
-	nextPosition = tileMap.GetNeighbor(position, direction)
-	
-	for button in moveButtons:
-		var buttonCell = tileMap.GetNeighbor(fromPosition, button.direction, false)
-		var indicatorValue = tileMap.CalculateIndicatorForCell(buttonCell)
-		button.SetText(str(indicatorValue))
-	
 	fromPosition = position
+	nextPosition = tileMap.GetNeighbor(position, direction)
 	nextPosition.y += 16
 	
+	UpdateCellIndicators()
+	
 	movement = 0.0
+	pass
+
+func UpdateCellIndicators():
+	var currentCell = tileMap.GetCellCoords(nextPosition + Vector2(32,32))
+	currentCellLabel.text = str(tileMap.CalculateIndicatorForCell(currentCell, false))
+	
+	for button in moveButtons:
+		var buttonCell = tileMap.GetNeighbor(nextPosition + Vector2(32,32), button.direction, false)
+		var indicatorValue = tileMap.CalculateIndicatorForCell(buttonCell)
+		button.SetText(str(indicatorValue))
 	pass
--- Scripts/TileGeneration.gd
+++ Scripts/TileGeneration.gd
@@ -2,14 +2,16 @@ extends TileMap
 
 var neighborDirectionIds
 var difficulty
-var bombLocations
+var bombCoords
+var exploredCoords
 
 var rng
 
 func _ready():
 	neighborDirectionIds = get_node("/root/GameData").get("neighborDirectionIds")
 	difficulty = get_node("/root/GameData").get("difficulty")
-	bombLocations = get_node("/root/GameData").get("bombLocations")
+	bombCoords = get_node("/root/GameData").get("bombCoords")
+	exploredCoords = get_node("/root/GameData").get("exploredCoords")
 	rng = RandomNumberGenerator.new()
 	var mapSize = get_node("/root/GameData").get("mapSize")
 	generate_cells(calculate_coords(mapSize), false)
@@ -27,13 +29,13 @@ func calculate_coords(mapSize: Vector2i):
 func generate_cells(coordsList: Array, allowOverwrite: bool = true):
 	for coords in coordsList:
 		if (!allowOverwrite and get_cell_tile_data(0, coords) != null):
-			return
+			continue
 		
 		var tile = 0
 		
 		var chance = rng.randf_range(0.0,100.0)
 		if (chance <= difficulty):
-			bombLocations.append(coords)
+			bombCoords.append(coords)
 			tile = 1
 			
 		set_cell(0, coords, 0, Vector2i(tile,0), 0)
@@ -48,16 +50,26 @@ func GetNeighbor(location: Vector2, direction: int = 0, global: bool = true):
 	return nextGlobalCell
 
 func GetGlobalPos(coords: Vector2i):
-	var globalPos = map_to_local(coords)
-	return globalPos
+	return map_to_local(coords)
+	
+func GetCellCoords(location: Vector2):
+	return local_to_map(location)
 
-func CalculateIndicatorForCell(coords: Vector2i):
+func CalculateIndicatorForCell(coords: Vector2i, checkExplored: bool = true):
+	if (!exploredCoords.has(coords) and checkExplored):
+		return "?"
+	
 	var surroundingBombs = 0
-	print(bombLocations)
-	print("Cell: " + str(coords))
 	for neighbor in get_surrounding_cells(coords):
-		print("Neighbor: " + str(neighbor))
-		if (bombLocations.has(neighbor)):
+		if (bombCoords.has(neighbor)):
 			surroundingBombs += 1
-	print("Found: " + str(surroundingBombs))
 	return surroundingBombs
+
+func OnEnterCell(location: Vector2):
+	var enteredCell = local_to_map(location)
+	if (bombCoords.has(enteredCell)):
+		print("GAME OVER")
+	else:
+		print("Safe cell")
+		exploredCoords.append(enteredCell)
+	pass
